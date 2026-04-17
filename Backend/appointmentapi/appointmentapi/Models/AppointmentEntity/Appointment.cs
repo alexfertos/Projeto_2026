d4f50a4f568c2c7243a41b7cf9922504297798d9
alexfertos <128864884+alexfertos@users.noreply.github.com>
2026-04-17 20:37:13 -0300

    Add files via upload

---
+++ Backend/appointmentapi/appointmentapi/Models/AppointmentEntity/Appointment.cs
@@ -0,0 +1,22 @@
+﻿using appointmentapi.Models.AuthEntity;
+
+namespace appointmentapi.Models.AppointmentEntity;
+
+public class Appointment
+{
+    public int Id { get; set; }
+    public int PersonId { get; set; }
+    public Person Person { get; set; }
+    public int SpecialtyId { get; set; }
+    public Specialty Specialty { get; set; }
+    public int UnitId { get; set; }
+    public Unit Unit { get; set; }
+    public int? AddressId { get; set; }
+    public Address Address { get; set; }
+    public int? PhoneId { get; set; }
+    public Phone Phone { get; set; }
+    public DateTime Date { get; set; }
+}
+
+//💡 Exemplo de dados:
+//João Silva, 2025-11-10 09:00, Especialidade: Cardiologia, Unidade: Paulista, Status: “Agendado”
---
+++ Backend/appointmentapi/appointmentapi/Models/AppointmentEntity/UnitSchedule.cs
@@ -0,0 +1,17 @@
+﻿namespace appointmentapi.Models.AppointmentEntity;
+
+public class UnitSchedule
+{
+    public int Id { get; set; }
+    public int UnitId { get; set; }
+    public Unit Unit { get; set; }
+    public int DayOfWeek { get; set; }
+    public int SlotDurationMinutes { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public bool IsAvailable { get; set; }
+}
+
+//💡 Exemplo de dados:
+//Segunda | 08:00 | 17:00 | Disponível ✅
+//Sábado | 08:00 | 12:00 | Indisponível ❌
