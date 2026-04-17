27ba2205f158827f2768a6d0811f95f20b6142f7
alexfertos <128864884+alexfertos@users.noreply.github.com>
2026-04-17 20:35:58 -0300

    Add files via upload

---
+++ Backend/appointmentapi/appointmentapi/Controllers/AppointmentController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace appointmentapi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AppointmentController : Controller
+{
+   
+}
